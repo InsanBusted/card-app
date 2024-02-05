a6fbd880d21054bde2b4f6e8049fe2532d614428
InsanBusted <insanbusted@gmail.com>
2024-02-06 00:51:27 +0700

    card app

---
+++ src/components/Card.jsx
@@ -0,0 +1,67 @@
+const Card = () => {
+  return (
+    <main id="content">
+      <section>
+        <div className="container height">
+          <div className="konten">
+            <div className="row column">
+              <div className="col-md-6 left">
+                <p className="text-white text-judul">
+                  Get <span className="insights">insights</span> that help your
+                  business grow.
+                </p>
+                <p className="sub-judul mt-5">
+                  Discover the benefits of data analytics and make better
+                  decisions regarding revenue customer experience, and overall
+                  efficiency.
+                </p>
+                <div className="info">
+                  <div className="mt-1 detail-info">
+                    <p className="text-info">
+                      10k+ <br />
+                      <span>COMPANIES</span>
+                    </p>
+                  </div>
+                  <div className="mt-1 detail-info">
+                    <p className="text-info">
+                      314 <br />
+                      <span>TEMPLATES</span>
+                    </p>
+                  </div>
+                  <div className="mt-1 detail-info">
+                    <p className="text-info">
+                      12M+ <br />
+                      <span>QUERIES</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 right">
+                <div className="latar">
+                  <p className="text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod ad similique ab! Est, culpa! Ab placeat reprehenderit
+                    sapiente culpa ducimus cumque. Dignissimos culpa, nisi
+                    recusandae nihil non repudiandae tenetur autem reprehenderit
+                    fugiat optio velit error id, sapiente incidunt architecto ab
+                    quo, aliquam porro voluptate ea. Doloremque animi doloribus
+                    veritatis nostrum quod repellat harum nulla optio commodi
+                    temporibus unde fugiat, voluptate dignissimos delectus!
+                    Earum sequi architecto voluptatum, ullam vitae, rerum
+                    voluptatibus sunt reiciendis distinctio quibusdam sed
+                    quaerat id reprehenderit labore iure exercitationem beatae
+                    molestias praesentium impedit adipisci pariatur? Sunt,
+                    repellendus distinctio quos qui incidunt commodi, architecto
+                    vel at eveniet aspernatur nisi.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export { Card };
